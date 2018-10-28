8f8fb3693b9f52f29a60bf508e89bd08037122d3
Hang Gao <cullengao@gmail.com>
2018-10-27 17:48:23 -0700

    Update

--- js/hidebib.js
+++ js/hidebib.js
@@ -12,31 +12,47 @@ function hideallbibs()
     }
 }
 
-function togglebib(paperid)
+function togglebib(paperId)
 {
-    var paper = document.getElementById(paperid) ;
+    var paper = document.getElementById(paperId) ;
     var bib = paper.getElementsByTagName('pre') ;
+    var block = document.getElementById(_transId(paperId))
     if (bib.length > 0) {
-        if (bib [0] .style.display == 'none') {
-            bib [0] .style.display = 'block' ;
+        if (bib[0].style.display == 'none') {
+            bib[0].style.display = 'block' ;
+            block.style.display = 'none';
         } else {
-            bib [0] .style.display = 'none' ;
+            bib[0].style.display = 'none' ;
         }
     }
 }
 
 function toggleblock(blockId)
 {
-   var block = document.getElementById(blockId);
-   if (block.style.display == 'none') {
-    block.style.display = 'block' ;
-   } else {
-    block.style.display = 'none' ;
-   }
+    var block = document.getElementById(blockId);
+    var bib = document.getElementById(
+        _transId(blockId)).getElementsByTagName('pre') ;
+    if (block.style.display == 'none') {
+        block.style.display = 'block' ;
+        if (bib.length > 0) {
+            bib[0].style.display = 'none' ;
+        }
+    } else {
+        block.style.display = 'none' ;
+    }
 }
 
 function hideblock(blockId)
 {
-   var block = document.getElementById(blockId);
-   block.style.display = 'none' ;
+    var block = document.getElementById(blockId);
+    block.style.display = 'none' ;
+}
+
+function _transId(id)
+{
+    if (id.endsWith('_abs')) {
+        return id.substr(0, id.length - 4)
+    } else {
+        return id + '_abs'
+    }
 }
