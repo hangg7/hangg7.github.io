f82f92f8fbae69fa01e34f96127e645e76523b3b
Hang Gao <hangg.sv7@gmail.com>
2020-06-14 12:30:27 -0700

    [master] anonymous commit

---
+++ set_access.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# File   : set_access.sh
+# Author : Hang Gao
+# Email  : hangg.sv7@gmail.com
+# Date   : 06/14/2020
+#
+# Distributed under terms of the MIT license.
+
+set -e -x
+
+# public resources
+chmod -R 755 css fonts js
+chmod 744 index.html
+
+# private but accessable at the end node
+chmod -R 755 projects
+chmod 701 projects
+chmod -R 755 misc
+chmod 701 misc
